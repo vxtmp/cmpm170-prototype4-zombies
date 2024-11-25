2bad34550066b36bb393f347e1196d17d9db18c3
ljcui1 <yangyangjiaocui@gmail.com>
2024-11-24 20:08:23 -0800

    adding health and hunger bar functionality that is able to be called

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private Image healthBar;
+    [SerializeField] private Image hungerBar;
+    public float healthAmt = 100f;
+    public float hungerAmt = 100f;
+    public float totalHealth = 100f;
+    public float totalHunger = 100f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +22,30 @@ public class UIManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void TakeDmg(float dmg)
+    {
+        healthAmt -= dmg;
+        healthBar.fillAmount = healthAmt / totalHealth;
+    }
+    public void TakeHngr(float hngr)
+    {
+        hungerAmt -= hngr;
+        hungerBar.fillAmount = hungerAmt / totalHunger;
+    }
+    public void Heal(float health)
+    {
+        healthAmt += health;
+        healthAmt = Mathf.Clamp(healthAmt, 0, 100);
+
+        healthBar.fillAmount = healthAmt / totalHealth;
+    }
+    public void Eat(float food)
+    {
+        hungerAmt += food;
+        hungerAmt = Mathf.Clamp(hungerAmt, 0, 100);
+
+        hungerBar.fillAmount = hungerAmt / totalHunger;
     }
 }
