21c97cb0d48a23bd8ee190ebe5dd582bcab46f7c
ljcui1 <yangyangjiaocui@gmail.com>
2024-12-04 17:20:57 -0800

    123456 hotkey inv navigational

--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -12,6 +12,7 @@ public class InventorySlot : MonoBehaviour
     private Button btn;
     private Color pressed;
     private string pressHex = "#C8C8C8";
+    public int numkey;
 
     [SerializeField] private PlayerBehavior playerBehavior;
 
@@ -22,6 +23,44 @@ public class InventorySlot : MonoBehaviour
         ColorUtility.TryParseHtmlString(pressHex, out Color color);
         pressed = color;
     }
+
+    void Update()
+    {
+        foreach (KeyCode key in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(key))
+            {
+                if (key >= KeyCode.Alpha0 && key <= KeyCode.Alpha9)
+                {
+                    if (Input.GetKeyDown(numkey.ToString()))
+                    {
+                        selected = true;
+
+                    }
+                    else
+                    {
+                        selected = false;
+                    }
+                }
+                break; // Stop after the first keypress is handled
+            }
+
+        }
+        if (selected == true)
+        {
+            ColorBlock colorBlock = btn.colors;
+            colorBlock.normalColor = pressed;
+            btn.colors = colorBlock;
+            playerBehavior.curItem = item;
+        }
+        else
+        {
+            ColorBlock colorBlock = btn.colors;
+            colorBlock.normalColor = Color.white;
+            btn.colors = colorBlock;
+        }
+    }
+
     public void AddItem(Item newItem)
     {
         item = newItem;
