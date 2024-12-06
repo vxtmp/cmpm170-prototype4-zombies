fbddbdd3e07f0e192f9713dd147c8c289dca84bd
norm z <115136784+vxtmp@users.noreply.github.com>
2024-12-06 11:41:09 -0800

    layer collision for humanbullet / walls

--- Assets/Scripts/Unit Behaviors/HumanScript.cs
+++ Assets/Scripts/Unit Behaviors/HumanScript.cs
@@ -12,7 +12,7 @@ public class HumanScript : MonoBehaviour
     // human inventory
     public List<Item> items = new List<Item>();
     [SerializeField] private GameObject ItemPrefab;
-    [SerializeField] private GameObject humanBulletPrefab;
+    [SerializeField] private GameObject humanBulletPrefab; 
     public int health = 10;
     public int healthDelta = 0;
     private bool healthChanged = false;
@@ -131,5 +131,10 @@ public class HumanScript : MonoBehaviour
         //Debug.Log("human takedmg zombie: " + damageValue);
         this.healthDelta -= damageValue;
         this.healthChanged = true;
+    }
+
+    public void shoot()
+    {
+        // spawn a prefab of the bullet with slight offset.
     }
 }
