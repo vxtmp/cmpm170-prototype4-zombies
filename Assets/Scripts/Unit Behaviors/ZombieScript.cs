50d7a754cbc68134a7603c01d5e23505374d486d
kvanarsd <kvanarsd@ucsc.edu>
2024-12-08 17:52:10 -0800

    Switch zombie attack from enter to stay

--- Assets/Scripts/Unit Behaviors/ZombieScript.cs
+++ Assets/Scripts/Unit Behaviors/ZombieScript.cs
@@ -123,7 +123,7 @@ public class ZombieScript : MonoBehaviour
 
         return direction;
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
 
         string collTag = collision.gameObject.tag;
